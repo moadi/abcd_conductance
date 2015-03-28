cdacfbbe7215c94fba7eba2cb0ff8feb19c1981c
Mohammad Adi <moadi@cisco.com>
2015-03-27 21:15:15 -0400

    Smart local opt, no threshold

--- driver.cpp
+++ driver.cpp
@@ -593,7 +593,7 @@ int main(int argc, char** argv)
 //	cout << "Number of edges = " << g.num_edges << "\n\n";
 //
 //	cout << "Number of vertices = " << g.num_vertices << "\n\n";
-
+    
 	Parameters p(g);
 
 	Helper helper(g);
@@ -1524,45 +1524,45 @@ void replace_nodes_modularity(Community& c, WeightedGraph& wg, Graph& g, std::ve
 //            std::cout << "Outdegree of node " << i+1 << " is " << best_outdegree << "\n";
 //            std::cout << "Inlinks for community " << curr_comm << " decreases to ";
             
-//            double curr_mod = wg.modularity(g);
-//            
-//            wg.vertex[curr_comm].in_links -= c.in_degree[i];
-//            
-////            std::cout << wg.vertex[curr_comm].in_links << "\n";
-//            
-//            wg.vertex[curr_comm].degree_sum -= g.vertex[i].degree;
-//            
-//            wg.vertex[curr_comm].origNodes.erase(std::remove(wg.vertex[curr_comm].origNodes.begin(),
-//                                                             wg.vertex[curr_comm].origNodes.end(),
-//                                                             i));
+            double curr_mod = wg.modularity(g);
+            
+            wg.vertex[curr_comm].in_links -= c.in_degree[i];
+
+//            std::cout << wg.vertex[curr_comm].in_links << "\n";
+            
+            wg.vertex[curr_comm].degree_sum -= g.vertex[i].degree;
+            
+            wg.vertex[curr_comm].origNodes.erase(std::remove(wg.vertex[curr_comm].origNodes.begin(),
+                                                             wg.vertex[curr_comm].origNodes.end(),
+                                                             i));
 
             c.n2c[i] = best_comm;
-//            int prev_in_degree = c.in_degree[i];
-//            c.in_degree[i] = best_outdegree;
-//            
-//            auto it = c.out_degree[i].find(best_comm);
-//            c.out_degree[i].erase(it);
+            int prev_in_degree = c.in_degree[i];
+            c.in_degree[i] = best_outdegree;
             
-//            c.out_degree[i].insert(std::make_pair(curr_comm, prev_in_degree));
-//            c.out_degree[i][curr_comm] = prev_in_degree;
+            auto it = c.out_degree[i].find(best_comm);
+            c.out_degree[i].erase(it);
+            
+            c.out_degree[i].insert(std::make_pair(curr_comm, prev_in_degree));
+            c.out_degree[i][curr_comm] = prev_in_degree;
             
 //            std::cout << "Inlinks for community " << best_comm << " increases to ";
             
-//            wg.vertex[best_comm].in_links += best_outdegree;
+            wg.vertex[best_comm].in_links += best_outdegree;
             
 //            std::cout << wg.vertex[best_comm].in_links << "\n";
             
-//            wg.vertex[best_comm].degree_sum += g.vertex[i].degree;
-//            wg.vertex[best_comm].origNodes.push_back(i);
-//
-//            
+            wg.vertex[best_comm].degree_sum += g.vertex[i].degree;
+            wg.vertex[best_comm].origNodes.push_back(i);
+
+            
 //            std::cout << "Modularity increased from " << curr_mod << " to ";
 //            std::cout << wg.modularity(g) << "\n\n\n";
 ////
 ////            wg.displayGraph();
 ////
-            c.reset_degrees();
-            c.recalc_degrees(finalEdges);
+//            c.reset_degrees();
+//            c.recalc_degrees(finalEdges);
             wg = c.rebuild_graph(finalEdges, g);
 //
 //            wg.vertex[best_comm].origNodes.push_back(i);
@@ -1570,61 +1570,61 @@ void replace_nodes_modularity(Community& c, WeightedGraph& wg, Graph& g, std::ve
             
             
             //update all neighbors of this node
-//            for (int j=0; j < g.vertex[i].degree; ++j)
-//            {
-//                int neighbor = g.vertex[i].neighbors[j];
-//                if (c.n2c[neighbor] == curr_comm)
-//                {
-//                    --c.in_degree[neighbor];
-//                    auto it = c.out_degree[neighbor].find(best_comm);
-//                    if (it != c.out_degree[neighbor].end())
-//                    {
-//                        ++it->second;
-//                    }
-//                    else
-//                    {
-//                        c.out_degree[neighbor][best_comm] = 1;
-//                    }
-//                }
-//                else
-//                {
-//                    auto it_1 = c.out_degree[neighbor].find(curr_comm);
-//                    
-//                    assert(it_1 != c.out_degree[neighbor].end());
-//                    
-//                    if (it_1 != c.out_degree[neighbor].end())
-//                    {
-//                        if (it_1->second-1 == 0)
-//                        {
+            for (int j=0; j < g.vertex[i].degree; ++j)
+            {
+                int neighbor = g.vertex[i].neighbors[j];
+                if (c.n2c[neighbor] == curr_comm)
+                {
+                    --c.in_degree[neighbor];
+                    auto it = c.out_degree[neighbor].find(best_comm);
+                    if (it != c.out_degree[neighbor].end())
+                    {
+                        ++it->second;
+                    }
+                    else
+                    {
+                        c.out_degree[neighbor][best_comm] = 1;
+                    }
+                }
+                else
+                {
+                    auto it_1 = c.out_degree[neighbor].find(curr_comm);
+                    
+                    assert(it_1 != c.out_degree[neighbor].end());
+                    
+                    if (it_1 != c.out_degree[neighbor].end())
+                    {
+                        if (it_1->second-1 == 0)
+                        {
 //                            std::cout << "Erasing community " << it_1->first << " from node " << neighbor << "\n\n";
-//                            c.out_degree[neighbor].erase(it_1);
-//                        }
-//                        else
-//                        {
-//                            --(it_1->second);
-//                            if (neighbor == 49)
-//                            {
+                            c.out_degree[neighbor].erase(it_1);
+                        }
+                        else
+                        {
+                            --(it_1->second);
+                            if (neighbor == 49)
+                            {
 //                                std::cout << "outdegree of node " << neighbor << " to comm " << curr_comm << " is " << it_1->second << "\n";
-//                            }
-//                        }
-//                        if (c.n2c[neighbor] == best_comm)
-//                        {
-//                            ++c.in_degree[neighbor];
-//                        }
-//            
-//                    }
-//                    auto it_2 = c.out_degree[neighbor].find(best_comm);
-//                    if (it_2 != c.out_degree[neighbor].end())
-//                    {
-//                        ++it_2->second;
-//                    }
-//                    else
-//                    {
-////                        std::cout << "Inserting community " << best_comm << " in node " << neighbor << "\n\n";
-//                        c.out_degree[neighbor][best_comm] = 1;
-//                    }
-//                }
-//            }
+                            }
+                        }
+                        if (c.n2c[neighbor] == best_comm)
+                        {
+                            ++c.in_degree[neighbor];
+                        }
+            
+                    }
+                    auto it_2 = c.out_degree[neighbor].find(best_comm);
+                    if (it_2 != c.out_degree[neighbor].end())
+                    {
+                        ++it_2->second;
+                    }
+                    else
+                    {
+//                        std::cout << "Inserting community " << best_comm << " in node " << neighbor << "\n\n";
+                        c.out_degree[neighbor][best_comm] = 1;
+                    }
+                }
+            }
         }
     }
 }
